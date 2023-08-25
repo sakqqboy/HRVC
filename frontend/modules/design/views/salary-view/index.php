0d0d70ef6f499e926be39007afda533a871b79d4
sudarat1997 <nuuta.33@gmail.com>
2023-08-25 16:20:32 +0700

    commit

---
+++ frontend/modules/design/views/salary-view/index.php
@@ -0,0 +1,65 @@
+<?php
+$this->title = 'view in details';
+?>
+
+<div class="col-12 mt-90">
+    <div class="alert alert-light">
+        <div class="row">
+            <div class="col-lg-5 col-md-6 col-12 alert bkc-salary">
+                <div class="col-12 data">
+                    Salary Increment Data
+                </div>
+                <div class="col-12 between">
+                    Between Date 08-02-2016 to 08-03-2023
+                </div>
+                <div class="col-12 pt-30">
+                    <table class="table">
+                        <thead class="table-secondary" style="border-radius: 3px;">
+                            <tr>
+                                <th scope="col">Working Year</th>
+                                <th scope="col">Total Salary</th>
+                                <th scope="col">Increase Ratio</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>1st</th>
+                                <th>10,000,000</th>
+                                <th>0.0%</th>
+                            </tr>
+                            <tr>
+                                <th>2nd</th>
+                                <th>10,000,000</th>
+                                <th>0.0%</th>
+                            </tr>
+                            <tr>
+                                <th>3rd</th>
+                                <th>11,000,000</th>
+                                <th>0.0%</th>
+                            </tr>
+                            <tr>
+                                <th>4th</th>
+                                <th>11,000,000</th>
+                                <th>0.0%</th>
+                            </tr>
+                            <tr>
+                                <th>5th</th>
+                                <th>12,000,000</th>
+                                <th>0.0%</th>
+                            </tr>
+                            <tr>
+                                <th>6th</th>
+                                <th>12,000,000</th>
+                                <th>9.0%</th>
+                            </tr>
+                            <tr>
+                                <th>6th</th>
+                                <th>12,000,000</th>
+                                <th>9.0%</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
