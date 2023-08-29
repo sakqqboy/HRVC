f7d326e5eac2b54e69a10c917c7692757f5fc72d
sakqqboy <saknakhngam@hotmail.com>
2023-08-29 17:59:09 +0700

    update

--- frontend/modules/setting/views/employee/evaluation.php
+++ frontend/modules/setting/views/employee/evaluation.php
@@ -10,32 +10,30 @@
 <div class="col-lg-12 col-md-12 col-12 pl-20 pr-20">
 	<div class="shadow p-3 bg-body rounded">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
-		<canvas id="myChart1"></canvas>
+		<canvas id="myChart" style="width: 100%;height:390px;"></canvas>
 		<script>
-			const xValues = ['1Q', '2Q', '3Q', '4Q', '1Q', '2Q', '3Q', '4Q', '1Q', '2Q', '3Q', '4Q', '1Q', '2Q', '3Q', '4Q', '1Q', '2Q', '3Q', '4Q'];
-			const yValues = ['0', '20', '30', '40', '50', '60', '70', '80', '90', '100']
+			const xValue = ['1Q', '2Q', '3Q', '4Q', '1Q', '2Q', '3Q', '4Q', '1Q', '2Q', '3Q', '4Q', '1Q', '2Q', '3Q', '4Q', '1Q', '2Q', '3Q', '4Q'];
+			const yValue = ['0', '20', '30', '40', '50', '60', '70', '80', '90', '100']
 
-			new Chart("myChart1", {
+			new Chart("myChart", {
 				type: "line",
 				data: {
-					labels: xValues,
+					labels: xValue,
 					datasets: [{
-							data: [60, 70, 60, 80, 70, 59, 50, 60, 70, 80, 70, 80, 70, 60, 70, 59, 70, 50, 70, 48],
+							data: [70, 60, 70, 60, 80, 70, 60, 50, 60, 70, 80, 70, 80, 70, 60, 70, 50, 70, 50, 55],
 							borderColor: "red",
 							lineTension: 0,
 							fill: false
 
-
 						}, {
-							data: [70, 60, 75, 65, 80, 60, 90, 60, 75, 57, 95, 50, 40, 30, 60, 85, 60, 60, 60, 50],
+							data: [60, 70, 60, 80, 70, 60, 50, 60, 70, 80, 70, 80, 70, 60, 70, 60, 70, 50, 70, 50],
 							borderColor: "orange",
 							lineTension: 0,
 							fill: false
 
 						},
 						{
-							data: [50, 45, 70, 40, 60, 55, 65, 70, 60, 70, 40, 45, 60, 50, 39, 70, 78, 75, 90, 93],
+							data: [40, 50, 40, 30, 28, 35, 45, 56, 40, 43, 60, 40, 50, 50, 59, 40, 43, 56, 50, 48],
 							borderColor: "blue",
 							lineTension: 0,
 							fill: false
