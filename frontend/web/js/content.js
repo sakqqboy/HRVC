d13ede0162608ee38ffca5a5f1b433ec33c436e1
sudarat1997 <nuuta.33@gmail.com>
2023-09-05 17:41:22 +0700

    commit

--- frontend/web/js/content.js
+++ frontend/web/js/content.js
@@ -14,3 +14,14 @@ items.forEach((number, index) => {
         }
     }, 15);
 });
+
+
+$(".animated-progress span").each(function () {
+    $(this).animate(
+        {
+            width: $(this).attr("data-progress") + "%",
+        },
+        1000
+    );
+    $(this).text($(this).attr("data-progress") + "%");
+});
