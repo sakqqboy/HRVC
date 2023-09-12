23c9d29545d81573481a9c1cb24f2a4251429104
sudarat1997 <nuuta.33@gmail.com>
2023-09-12 16:15:04 +0700

    commit

--- frontend/modules/design/views/Kfi/index.php
+++ frontend/modules/design/views/Kfi/index.php
@@ -1921,8 +1921,7 @@ $this->title = 'KFI';
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Understood</button>
+                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
