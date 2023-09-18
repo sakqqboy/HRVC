4b8a5eb876980b48ead049b98fbfaef71e781972
sudarat1997 <nuuta.33@gmail.com>
2023-09-18 19:27:04 +0700

    commit

--- frontend/modules/design/views/single-kgi/index.php
+++ frontend/modules/design/views/single-kgi/index.php
@@ -123,7 +123,7 @@ $this->title = 'Single KGI';
                             <td>2nd Feb, 2023</td>
                             <td>23rd Feb, 2023</td>
                             <td colspan="row">
-                                <span data-bs-toggle="modal" data-bs-target="#exampleModal"> <img src="<?= Yii::$app->homeUrl ?>image/comment.png" class="comment-td-dropdown"></span>
+                                <span data-bs-toggle="modal" data-bs-target="#exampleModalcomment3"> <img src="<?= Yii::$app->homeUrl ?>image/comment.png" class="comment-td-dropdown"></span>
                                 <span class="dropdown menulink" href="#" role="but ton" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false"> <i class="fa fa-ellipsis-v on-cursor" aria-hidden="true"></i> </span>
                                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
                                     <li data-bs-toggle="modal" data-bs-target="#exampleModal3">
@@ -314,7 +314,99 @@ $this->title = 'Single KGI';
 </div>
 
 <!-- modal comment single kgi -->
-
+<div class="modal fade" id="exampleModalcomment3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header" style="border-bottom:none;">
+                <div class="modal-title Modalfirstone" id="exampleModalcommentLabel"><i class="fa fa-clock-o" aria-hidden="true"></i> Work More and More</div>
+                <div class="modal-title Modalfirstone">Tokyo Consulting Firm Limited</div>
+            </div>
+            <div class="fsm">Dhaka, Bangladesh <img src="<?= Yii::$app->homeUrl ?>image/is.jpg" class="Round1"></div>
+            <div class="modal-body">
+                <ul class="nav nav-pills mb-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-12">
+                            <a class="link-3" id="v-pills-Issues-tab" data-bs-toggle="pill" data-bs-target="#v-pills-Issues" type="button" role="tab" aria-controls="v-pills-Issues" aria-selected="true">Issues</a>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-12">
+                            <a class="link-3" id="v-pills-History-tab" data-bs-toggle="pill" data-bs-target="#v-pills-History" type="button" role="tab" aria-controls="v-pills-History" aria-selected="false">History</a>
+                        </div>
+                    </div>
+                </ul>
+                <hr>
+                <div class="tab-content" id="v-pills-tabContent">
+                    <div class="tab-pane fade show active" id="v-pills-Issues" role="tabpanel" aria-labelledby="v-pills-Issues-tab">
+                        <ul>
+                            <li class="li-circle">
+                                <img src="<?= Yii::$app->homeUrl ?>image/ehsan-small.png" class="image-circle1"><span class="li-name">Quazi Ehsan Hossain
+                                    <span class="Report-Issue"> May 31, 2023 <i class="fa fa-exclamation-triangle text-warning" aria-hidden="true"></i> </span>
+                            </li>
+                            <div class="style-circle-li">
+                                <span> Hi Tani! Could you take quick look at these Landing Page designs? when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
+                                    <p class="mt-20">Thanks so much!</p>
+                                </span>
+                            </div>
+                            <div class="alert alert-secondary" style="border: none;margin-top: -30px;margin-left:20px;">
+                                <div class="row">
+                                    <div class="col-lg-7 col-md-6 col-12">
+                                        <span class="badge bg-white"> <img src="<?= Yii::$app->homeUrl ?>image/pdf.png" class="pdf-down"></span> <span class="text-dark"> 115 IHI July Invoice(Gunman) 30.July.2021.pdf <span class="text-secondary font-size-12"> 2.3mb</span></span>
+                                    </div>
+                                    <div class="col-lg-5 col-md-6 col-12 text-end">
+                                        <button class="btn btn-outline-secondary"> <i class="fa fa-eye" aria-hidden="true"></i></button>
+                                        <button class="btn btn-outline-secondary"> <i class="fa fa-cloud-download" aria-hidden="true"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                            </li>
+                            <li class="li-circle1">
+                                <div class="col-12 card-hashed">
+                                    <img src="<?= Yii::$app->homeUrl ?>image/Watanabe.png" class="image-hashed">&nbsp; <span class="font-size-12">Tadawoki Watanabe</span>
+                                    <div class="col-12 problemm">
+                                        <span> Hi Tani! Could you take quick look at these Landing Page designs? when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
+                                            <p class="mt-20">Thanks so much!</p>
+                                        </span>
+                                    </div>
+                                    <div class="col-12 mt-20">
+                                        <div class="input-group pr-20">
+                                            <input for class="form-control">
+                                            <a><i class="fa fa-paperclip clip-file" aria-hidden="true" type="file"></i></a>
+                                            <button class="btn btn-primary form-submitbotton" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i> Submit</button>
+                                        </div>
+                                        <div class="mt-20"></div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="tab-pane fade" id="v-pills-History" role="tabpanel" aria-labelledby="v-pills-History-tab">
+                        <ul>
+                            <li class="li-circle">
+                                <img src="<?= Yii::$app->homeUrl ?>image/ehsan-small.png" class="image-circle1"><span class="li-name">Quazi Ehsan Hossain
+                                    <span class="Report-Issue">2:56 PM, May 31, 2023</span>
+                            </li>
+                            <div class="style-circle-li">
+                                <span> Hi Tani! Could you take quick look at these Landing Page designs? when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
+                                    <p class="mt-20">Thanks so much!</p>
+                                </span>
+                            </div>
+                            <div class="col-12 badge-pdf0">
+                                <span class="badge bg-light"> <img src="<?= Yii::$app->homeUrl ?>image/pdf.png" class="pdf-down"></span> <span class="text-dark"> 115 IHI July Invoice(Gunman) 30.July.2021.pdf 2.3mb</span>
+                            </div>
+                            <li class="li-circle">
+                                <img src="<?= Yii::$app->homeUrl ?>image/Watanabe.png" class="image-hashed">&nbsp; <strong class="font-size-14 text-dark">Tadawoki Watanabe </strong>&nbsp; <span class="Report-Issue"> 2:56 PM, May 31, 2023</span>
+                                <div class="col-12 problemm">
+                                    <span> Hi Tani! Could you take quick look at these Landing Page designs? when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
+                                        <p class="mt-20">Thanks so much!</p>
+                                    </span>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- end -->
 
 <!-- modal view single kgi -->
