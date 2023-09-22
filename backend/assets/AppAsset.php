5e1f32b3772305f1c9ab6052cea08cd4bd4a1bcf
sudarat1997 <nuuta.33@gmail.com>
2023-09-22 11:11:16 +0700

    commit

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,8 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
-        'css/home/login.css'
+        'css/site.css'
 
     ];
     public $js = [
