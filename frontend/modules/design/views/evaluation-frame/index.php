43f93f4673a33e794aa17b78ac391d5e545854b4
sudarat1997 <nuuta.33@gmail.com>
2023-12-13 08:50:02 +0700

    commit

--- frontend/modules/design/views/evaluation-frame/index.php
+++ frontend/modules/design/views/evaluation-frame/index.php
@@ -112,7 +112,175 @@ $this->title = 'Evaluation Frame';
                     <div class="col-lg-8">
                         <div class="alert alert-Evaluator">
                             <div class="col-12">
-                                1.234555859
+                                <div class="stepper-wrapper">
+                                    <span class="ontop">
+                                        <div class="Start-Date">Start Date</div>
+                                        <img src="<?= Yii::$app->homeUrl ?>image/mon.png" class="imagesmon">
+                                    </span>
+                                    <div class="stepper-item completed">
+                                        <div class="step-counter">E1</div>
+                                    </div>
+                                    <div class="stepper-item completed">
+                                        <div class="step-counter">E2</div>
+                                    </div>
+                                    <div class="stepper-item completed">
+                                        <div class="step-counter">E3</div>
+                                    </div>
+                                    <div class="stepper-item completed">
+                                        <div class="step-counter">E4</div>
+                                    </div>
+                                    <div class="stepper-item counter">
+                                        <div class="stepber"></div>
+                                    </div>
+                                    <span class="bottomon">
+                                        <div class="Start-Date">End Date</div>
+                                        <img src="<?= Yii::$app->homeUrl ?>image/mon.png" class="imagesmon">
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="alert alert-dashboard">
+                            <div class="col-12">
+                                <div class="row rowTearmduration">
+                                    <div class="col-2">TERMS</div>
+                                    <div class="col-5">START</div>
+                                    <div class="col-3">FINISH</div>
+                                    <div class="col-1">DURATION</div>
+                                </div>
+                                <div class="col-12 mt-20">
+                                    <div class="alert p-2 trantRankColor">
+                                        <div class="row settingDays">
+                                            <div class="col-lg-2">
+                                                <div class="col-12 Startsettingsolid">
+                                                    Goal Settings
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-5">
+                                                <div class="row">
+                                                    <div class="col-7">
+                                                        11th November 2023
+                                                    </div>
+                                                    <div class="col-4">
+                                                        <span class="badge bg-clder">
+                                                            <img src="/HRVC/frontend/web/images/icons/Dark/48px/Calender.png" class="cenler-arrows">
+                                                            <img src="/HRVC/frontend/web/image/arrowss.png" class="iconsarrowss">
+                                                            <img src="/HRVC/frontend/web/images/icons/Dark/48px/Calender.png" class="cenler-arrows">
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-3">
+                                                <div class="col-12">
+                                                    11th November 2023
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-2">
+                                                <div class="col-12 Startsettingsolid-left pl-5">
+                                                    65 Days
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="alert p-2 trantRankColor">
+                                        <div class="row settingDays">
+                                            <div class="col-lg-2">
+                                                <div class="col-12 Startsettingsolid">
+                                                    Self Submission
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-5">
+                                                <div class="row">
+                                                    <div class="col-7">
+                                                        11th November 2023
+                                                    </div>
+                                                    <div class="col-4">
+                                                        <span class="badge bg-clder">
+                                                            <img src="/HRVC/frontend/web/images/icons/Dark/48px/Calender.png" class="cenler-arrows">
+                                                            <img src="/HRVC/frontend/web/image/arrowss.png" class="iconsarrowss">
+                                                            <img src="/HRVC/frontend/web/images/icons/Dark/48px/Calender.png" class="cenler-arrows">
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-3">
+                                                <div class="col-12">
+                                                    11th November 2023
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-2">
+                                                <div class="col-12 Startsettingsolid-left pl-5">
+                                                    65 Days
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="alert p-2 trantYellowColor">
+                                        <div class="row settingDays">
+                                            <div class="col-lg-2">
+                                                <div class="col-12 Startsettingsolid">
+                                                    Mid Meeting
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-5">
+                                                <div class="row">
+                                                    <div class="col-7">
+
+                                                    </div>
+                                                    <div class="col-4">
+                                                        <span class="badge bg-clder">
+                                                            <img src="/HRVC/frontend/web/images/icons/Dark/48px/Calender.png" class="cenler-arrows">
+                                                            <img src="/HRVC/frontend/web/image/arrowss.png" class="iconsarrowss">
+                                                            <img src="/HRVC/frontend/web/images/icons/Dark/48px/Calender.png" class="cenler-arrows">
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-3">
+                                                <div class="col-12">
+
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-2">
+                                                <div class="col-12 Startsettingsolid-left pl-5">
+                                                    0 Days
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="alert p-2 trantRankColor">
+                                        <div class="row settingDays">
+                                            <div class="col-lg-2">
+                                                <div class="col-12 Startsettingsolid">
+                                                    Goal Settings
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-5">
+                                                <div class="row">
+                                                    <div class="col-7">
+                                                        11th November 2023
+                                                    </div>
+                                                    <div class="col-4">
+                                                        <span class="badge bg-clder">
+                                                            <img src="/HRVC/frontend/web/images/icons/Dark/48px/Calender.png" class="cenler-arrows">
+                                                            <img src="/HRVC/frontend/web/image/arrowss.png" class="iconsarrowss">
+                                                            <img src="/HRVC/frontend/web/images/icons/Dark/48px/Calender.png" class="cenler-arrows">
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-3">
+                                                <div class="col-12">
+                                                    11th November 2023
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-2">
+                                                <div class="col-12 Startsettingsolid-left pl-5">
+                                                    7 Days
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
