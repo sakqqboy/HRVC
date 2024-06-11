3be8e228b4fd84c3a6335e225a027f8210315a46
sudarat1997 <nuuta.33@gmail.com>
2024-06-11 17:55:35 +0700

    update

--- frontend/modules/design/views/individual-pim/index.php
+++ frontend/modules/design/views/individual-pim/index.php
@@ -7,5 +7,86 @@ $this->title = 'Individual Procress';
 ?>
 
 <div class="col-12 mt-70 environment pt-10 pr-10 pl-10">
+    <div class="bg-white pl-5 pr-5" style="border-radius: 5px;">
+        <div class="individual_pim_step1">
+            <div class="row">
+                <div class="col-lg-2 col-md-6 col-sm-6 col-12">
+                    <div class="col-12 MY_PIM_PANEL">
+                        MY PIM PANEL
+                    </div>
+                    <hr>
+                    <div class="col-12">
+                        <select class="form-select pt-5 pb-5" style="border-radius: 30px;font-size:13px;" aria-label="Default select example">
+                            <option selected value="">this select</option>
+                            <option value="1"> One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12 border-left">
+                    <div class="badge button1_dualpim">
+                        <div class="color-004DB0-E2">E2</div>
+                        <div class="color-004DB0-none">2022</div>
+                    </div>
+                    <span class="col-12 text-secondary">
+                        Final Evaluation Phase
+                    </span>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div class="col-12">
+                        fmgp
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                    fgdrtfu
+                </div>
+            </div>
+        </div>
 
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                <div class="individual_pim_step1 pt-10 pb-10">
+                    <div class="col-3 badge pim1_formatnumkfi">
+                        KFI &nbsp; <span class="text-danger"><?= number_format(60) ?>%</span>
+                    </div>
+                    <div class="col-12 mt-10">
+                        <table class="table table-white">
+                            <tr>
+                                <td>
+                                    <img src="<?= Yii::$app->homeUrl ?>images/icons/Dark/48px/linegroup-4.png" class="pim1-imageslinegroup"> <span class="font-size-11 text-secondary font-b">Sales</span>
+                                </td>
+                                <td>
+                                    Priority
+                                </td>
+                                <td>
+                                    target 456686
+                                </td>
+                                <td>
+                                    Update
+                                </td>
+                                <td>
+                                    21-Dec-23
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                <div class="individual_pim_step1">
+                    <div class="col-12">
+                        ghfgj
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                <div class="individual_pim_step1">
+                    <div class="col-12">
+                        ghfgjfghgyf
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
