cf556bc6fd61a513a3ab1243eeafcbdc09ea5049
sakqqboy <saknakhngam@hotmail.com>
2024-11-08 09:25:22 +0700

    Update wait_approve_employee.php

--- frontend/modules/kgi/views/management/wait_approve_employee.php
+++ frontend/modules/kgi/views/management/wait_approve_employee.php
@@ -25,6 +25,7 @@ $this->title = 'Waiting for approve KGI';
 					<th class="text-center">New</th>
 					<th class="text-center">Change</th>
 					<th class="text-center">Month</th>
+					<th>Reason</th>
 					<th class="text-center"></th>
 					<th></th>
 				</tr>
@@ -56,15 +57,20 @@ $this->title = 'Waiting for approve KGI';
 							<td class="font-b text-end"><?= number_format($employeeKgi["target"], 2) ?></td>
 							<td class="font-b text-end"><?= number_format($employeeKgi["newTarget"], 2) ?></td>
 							<td class="text-danger text-end"><?= number_format($employeeKgi["newTarget"] - $employeeKgi["target"], 2) ?></td>
-							<td><?= $employeeKgi["month"] ?></td>
+							<td class="text-center"><?= $employeeKgi["month"] ?></td>
 							<td style="width: 30%;"><?= $employeeKgi["reson"] ?></td>
+							<td> <a href="javascript:approveTargetKgiTeam(<?= $kgiEmployeeHistoryId ?>,1)" class="approve-btn mr-5 no-underline mr-10">
+									<img src="<?= Yii::$app->homeUrl ?>images/icons/Settings/circle-check-blue.svg" class="mr-5" style="margin-top: -2px;">Approve
+								</a>
+								<a href="javascript:approveTargetKgiTeam(<?= $kgiEmployeeHistoryId ?>,0)" class="decline-btn  no-underline">
+									<img src="<?= Yii::$app->homeUrl ?>images/icons/Settings/circle-cancel.svg" class="mr-5" style="margin-top: -2px;">Decline
+								</a>
+							</td>
 							<td class="text-center">
 								<a href="<?= Yii::$app->homeUrl ?>kgi/management/approve-kgi-employee/<?= ModelMaster::encodeParams(["kgiEmployeeHistoryId" => $kgiEmployeeHistoryId]) ?>" class="btn btn-sm btn-primary font-size-10">
 									<i class="fa fa-eye" aria-hidden="true"></i>
 								</a>
 							</td>
-							<th class="text-center"></th>
-							<th></th>
 						</tr>
 					<?php
 					endforeach;
