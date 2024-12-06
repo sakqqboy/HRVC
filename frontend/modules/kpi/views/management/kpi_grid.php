2befd380050152b2927c57c28ea863515867c386
sakqqboy <saknakhngam@hotmail.com>
2024-12-06 10:54:31 +0700

    Update kpi_grid.php

--- frontend/modules/kpi/views/management/kpi_grid.php
+++ frontend/modules/kpi/views/management/kpi_grid.php
@@ -176,12 +176,11 @@ $this->title = 'KPI Grid View';
                                                         <div class="col-5 pr-2 pl-13">
                                                             <div class="row d-flex align-items-center" style="min-height: 24px;">
                                                                 <?php if ($kpi["countEmployee"] != 0) {
-
-                                                                    //throw new Exception(print_r($kpi["kpiEmployee"], true))
                                                                 ?>
                                                                     <div class="col-2">
                                                                         <?php if (isset($kpi['kpiEmployee'][0])):
-                                                                            if (file_exists(Yii::$app->homeUrl . $kpi['kpiEmployee'][0]) == 0) {
+                                                                            $filePath = Yii::getAlias('@webroot') . '/' . $kpi['kpiEmployee'][0];
+                                                                            if (file_exists($filePath) == 0) {
                                                                                 $kpi['kpiEmployee'][0] = 'image/user.svg';
                                                                             }
                                                                         ?>
@@ -191,7 +190,8 @@ $this->title = 'KPI Grid View';
                                                                     </div>
                                                                     <div class="col-2 pic-after pt-0">
                                                                         <?php if (isset($kpi['kpiEmployee'][1])):
-                                                                            if (file_exists(Yii::$app->homeUrl . $kpi['kpiEmployee'][1]) == 0) {
+                                                                            $filePath = Yii::getAlias('@webroot') . '/' . $kpi['kpiEmployee'][1];
+                                                                            if (file_exists($filePath) == 0) {
                                                                                 $kpi['kpiEmployee'][1] = 'image/user.png';
                                                                             }
                                                                         ?>
@@ -201,7 +201,8 @@ $this->title = 'KPI Grid View';
                                                                     </div>
                                                                     <div class="col-2 pic-after pt-0">
                                                                         <?php if (isset($kpi['kpiEmployee'][2])):
-                                                                            if (file_exists(Yii::$app->homeUrl . $kpi['kpiEmployee'][2]) == 0) {
+                                                                            $filePath = Yii::getAlias('@webroot') . '/' . $kpi['kpiEmployee'][2];
+                                                                            if (file_exists($filePath) == 0) {
                                                                                 $kpi['kpiEmployee'][2] = 'image/user.png';
                                                                             }
                                                                         ?>
