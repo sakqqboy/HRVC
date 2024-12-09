c936c5dc6f05e62fc792104b39eb3270d2b05428
phumapanther <165149554+phumapanther@users.noreply.github.com>
2024-12-09 15:11:41 +0700

    update

--- frontend/modules/kfi/views/management/index_grid.php
+++ frontend/modules/kfi/views/management/index_grid.php
@@ -372,6 +372,13 @@ $this->title = 'KFI Grid View';
                                                          ?>
                                         </div>
                                         <?php
+                                                } else { ?>
+                                        <div class="pim-btn-disable" data-bs-target="#update-kgi-modal">
+                                            <img src="<?= Yii::$app->homeUrl ?>images/icons/Settings/locked.svg"
+                                                style="width: 12px; height: 12px;"> Locked
+                                        </div>
+                                        <?php
+
                                                 }
                                                 ?>
                                     </div>
@@ -383,222 +390,14 @@ $this->title = 'KFI Grid View';
                                     </div>
                                 </div>
                             </div>
-                            <!-- <div class="col-lg-5 pim-subheader-font mt-5">
-                                <div class="row">
-                                    <div class="col-lg-6 col-md-6 col-12 pr-3 pl-20">
-                                        <div class="col-12 head-letter head-<?= $colorFormat ?>">Issue</div>
-                                        <div class="col-12 body-letter body-letter-<?= $colorFormat ?>">
-                                            Now use Lorem Ipsum as their default model text, and a search for 'lorem
-                                            ipsum' will uncover many web sites still in their infancy. Various versions.
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6 col-md-6 col-12 pl-5 pr-20">
-                                        <div class="col-12 head-letter head-<?= $colorFormat ?>">Solution</div>
-                                        <div class="col-12 body-letter body-letter-<?= $colorFormat ?>">Now use Lorem
-                                            Ipsum as their default model text, and a search for 'lorem ipsum' will
-                                            uncover many web sites still in their infancy. Various versions.
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div> -->
                         </div>
                     </div>
-                    <!-- <div class="col-lg-4 col-md-6 col-sm-5 col-12 mt-20">
-								<div class="col-12 border pl-20 pr-20 pt-10 pb-5 <?php // $kfi['isOver'] == 1 ? 'bg-over' : 'bg-white' 
-                                                                                    ?>" style="border-radius:10px;box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;">
-									<div class="row">
-										<div class="col-12">
-											<div class="row">
-												<div class="col-12 text-end pr-0">
-													<a class="btn btn-xs btn-outline-secondary pt-0" style="margin-left: 0px;" data-bs-toggle="modal" data-bs-target="#staticBackdrop3" onclick="javascript:kfiHistory(<?= $kfiId ?>)">
-														<i class="fa fa-eye mt-6" aria-hidden="true" style="margin-left: -5px"></i>
-													</a>
-													<?php
-                                                    //if ($role >= 5) {
-                                                    ?>
-														<a class="btn-xs  ml-5 pt-0" data-bs-toggle="modal" data-bs-target="#staticBackdrop4" onclick="javascript:prepareDeleteKfi(<?php //  $kfiId 
-                                                                                                                                                                                    ?>)">
-															<i class="fa fa-trash-o mt-6" aria-hidden="true" style="margin-left: -5px;"></i>
-														</a>
-													<?php
-                                                    //	}
-                                                    ?>
-												</div>
-												<div class="col-9 linechart-increase" style="margin-top: -25px;">
-													<i class="fa fa-line-chart mr-5" aria-hidden="true"></i> <?php //$kfi["kfiName"] 
-                                                                                                                ?>
-													<div class="col-12  ting-size mt-5">
-														<?php // $kfi["companyName"] 
-                                                        ?>
-													</div>
-													<div class="col-12 font-size-12 mt-5">
-														<img src="<?php // Yii::$app->homeUrl 
-                                                                    ?><?php //  $kfi["flag"] 
-                                                                        ?>" class="image-is mr-3"> <?php //  $kfi["branchName"] 
-                                                                                                    ?>
-													</div>
 
-												</div>
-											</div>
-										</div>
-										<div class="col-12 pl-0 text-end  pr-5 " style="margin-top:-35px;">
-											<div class="offset-6 col-6 text-end pl-0 pr-0">
-												<span class="badge rounded-pill bg-deadline0 ml-5" style="width:100%;">
-													<span class="deadline-orange"> Term</span>
-													<span class="font-size-10 text-dark" style="font-weight: 700;">: <?php //  $kfi['fromDate'] == "" ? 'Not set' : $kfi['fromDate'] 
-                                                                                                                        ?> - </span>
-													<span class="font-size-10 text-dark" style="font-weight: 700;"><?php //  $kfi['toDate'] == "" ? 'Not set' : $kfi['toDate'] 
-                                                                                                                    ?></span>
-												</span>
-											</div>
-										</div>
-										<div class="col-12 text-end pr-0 " style="margin-top:-8px;">
-											<span class="badge rounded-pill <?php //  $kfi['status'] == 1 ? 'bg-warning text-dark' : 'bg-success' 
-                                                                            ?>"> <?php //  $kfi['status'] == 1 ? 'In process' : 'Completed' 
-                                                                                    ?></span>
-										</div>
-										<div class="col-12 mt-15">
-											<div class="row">
-												<div class="col-lg-1 col-md-6 col-2 font-size-14 pt-30 pr-0 pl-0 text-center" style="font-weight: 500;">
-													<?php //  strtoupper(substr($kfi['month'], 0, 3)) 
-                                                    ?>
-												</div>
-												<div class="col-lg-4 col-md-6 col-3">
-													<div class="col-12 Quant-ratio">
-														Quant Ratio
-													</div>
-													<div class="col-12 font-size-10 mt-3">
-														<i class="fa fa-diamond" aria-hidden="true"></i> <?php // $kfi["quantRatio"] == 1 ? 'Quantity' : 'Quality' 
-                                                                                                            ?>
-													</div>
-												</div>
-
-												<div class="col-lg-3 col-md-6 col-3">
-													<div class="col-12 bullseye-con">
-														<i class="fa fa-bullseye" aria-hidden="true"></i> Target
-													</div>
-													<div class="col-12 million-number" style="font-weight: 500;">
-														<?php
-                                                        //$decimal = explode('.', $kfi["target"]);
-                                                        // if (isset($decimal[1])) {
-                                                        // 	if ($decimal[1] == '00') {
-                                                        // 		$show = number_format($decimal[0]);
-                                                        // 	} else {
-                                                        // 		$show = number_format($kfi["target"], 2);
-                                                        // 	}
-                                                        // } else {
-                                                        // 	$show = number_format($kfi["target"]);
-                                                        // }
-                                                        ?>
-														<?php //  $show 
-                                                        ?><?php //  $kfi["amountType"] == 1 ? '%' : '' 
-                                                            ?>
-													</div>
-												</div>
-												<div class="col-lg-1 col-md-6 col-3 pt-10  text-center">
-
-													<?php //  $kfi["code"] 
-                                                    ?>
-												</div>
-												<div class="col-lg-3 cl-md-6 col-3">
-													<div class="col-12 trophy-con">
-														<i class="fa fa-trophy" aria-hidden="true"></i> Result
-													</div>
-													<div class="col-12 million-number" style="font-weight: 500;">
-														<?php
-                                                        // if ($kfi["result"] != '') {
-                                                        // 	$decimalResult = explode('.', $kfi["result"]);
-                                                        // 	if (isset($decimalResult[1])) {
-                                                        // 		if ($decimalResult[1] == '00') {
-                                                        // 			$showResult = number_format($decimalResult[0]);
-                                                        // 		} else {
-                                                        // 			$showResult = number_format($kfi["result"], 2);
-                                                        // 		}
-                                                        // 	} else {
-                                                        // 		$showResult = number_format($kfi["result"]);
-                                                        // 	}
-                                                        // } else {
-                                                        // 	$showResult = 0;
-                                                        // }
-                                                        ?>
-														<?php //  $showResult 
-                                                        ?><?php //  $kfi["amountType"] == 1 ? '%' : '' 
-                                                            ?>
-													</div>
-												</div>
-
-												<div class="col-lg-1 col-md-6 col-6"></div>
-												<div class="col-lg-4 col-md-6 col-6">
-													<div class="col-12 padding-update">
-														Update Interval
-													</div>
-													<div class="col-12 update-mouth mt-5">
-														<?php //  $kfi["unit"] 
-                                                        ?>
-													</div>
-												</div>
-												<div class="col-lg-7 col-md-6 col-6 pr-0">
-													<div class="col-12 pr-0 pt-8">
-														<?php
-                                                        // $percent = explode('.', $kfi['ratio']);
-                                                        // if (isset($percent[1])) {
-                                                        // 	if ($percent[1] != '00') {
-                                                        // 		$showPercent = $kfi['ratio'];
-                                                        // 	} else {
-                                                        // 		$showPercent = $percent[0];
-                                                        // 	}
-                                                        // } else {
-                                                        // 	$showPercent = $percent[0];
-                                                        // }
-                                                        ?>
-														<div class="progress">
-															<div class="progress-bar" style="width:<?php //  $showPercent 
-                                                                                                    ?>%; background:#2F80ED;"></div>
-															<span class="badge rounded-pill  pro-load0"><?php //  $showPercent 
-                                                                                                        ?>%</span>
-														</div>
-													</div>
-												</div>
-											</div>
-										</div>
-										<div class="col-12" style="margin-top:-10px;">
-											<div class="row">
-												<div class="col-lg-12 text-end pr-0">
-													<span data-bs-toggle="modal" data-bs-target="#kfi-issue" onclick="javascript:showKfiComment(<?php // $kfiId 
-                                                                                                                                                ?>)">
-														<img src="<?php //  Yii::$app->homeUrl 
-                                                                    ?>image/comment.svg" class="comment-ima" style="margin-top: -5px;cursor:pointer;">
-													</span>&nbsp;&nbsp;
-													<?php
-                                                    //if ($role >= 5) {
-                                                    ?>
-														<span class="next-update-span" data-bs-toggle="modal" data-bs-target="#staticBackdrop2" onclick="javascript:updateKfi(<?= $kfiId ?>)">
-															<i class="fa fa-pencil-square-o font-size-19" aria-hidden="true"></i>
-														</span> &nbsp;
-													<?php
-                                                    //}
-                                                    ?>
-													<span class="text-primary font-size-12">Next Update</span>
-													<strong class="font-size-12 <?php // $kfi['nextCheck'] == "" || $kfi['isOver'] == 1 ? 'text-danger' : '' 
-                                                                                ?>">
-														<php //  $kfi['nextCheck'] == "" ? 'Not set' : $kfi['nextCheck'] ?>
-													</strong>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div> -->
                     <?php
                         endforeach;
                     }
                     ?>
                 </div>
-                <!-- </div>
-					</div>
-				</div>
-			</div> -->
             </div>
         </div>
         <input type="hidden" value="create" id="acType">
