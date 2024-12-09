3fbb169fa0af2d2a41d3d66ed034d043c0faacbd
phumapanther <165149554+phumapanther@users.noreply.github.com>
2024-12-09 10:27:35 +0700

    update

--- frontend/modules/kpi/views/kpi-personal/filter_list.php
+++ frontend/modules/kpi/views/kpi-personal/filter_list.php
@@ -7,7 +7,7 @@ use frontend\models\hrvc\Employee;
 use frontend\models\hrvc\Team;
 
 ?>
-<div class="row">
+<div class="row justify-content-end">
     <!-- <div class="col-1 pr-10 text-end">
 		<span>
 			<img src="<?= Yii::$app->homeUrl ?>images/icons/Dark/48px/FilterPlus.svg" class="pim-search-icon" style="cursor: pointer;" onclick="javascript:kpiFilterForEmployee()">
