e7bf2a54d6773ae8cb8e8b1829cece07fb3574f8
sakqqboy <saknakhngam@hotmail.com>
2024-12-20 16:35:07 +0700

    Update DefaultController.php

--- backend/modules/home/controllers/DefaultController.php
+++ backend/modules/home/controllers/DefaultController.php
@@ -2,11 +2,20 @@
 
 namespace backend\modules\home\controllers;
 
+use backend\models\hrvc\Branch;
+use backend\models\hrvc\Employee;
+use backend\models\hrvc\KgiTeam;
+use backend\models\hrvc\KgiTeamHistory;
 use yii\web\Controller;
 
 /**
  * Default controller for the `home` module
  */
+header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 class DefaultController extends Controller
 {
     /**
@@ -16,5 +25,15 @@ class DefaultController extends Controller
     public function actionIndex()
     {
         return $this->render('index');
+    }
+    public function actionPendingApproval($role, $employeeId)
+    {
+        $kgiTeam = KgiTeam::find()
+            ->select('kgiId')
+            ->where(["status" => 88])
+            ->groupBy('kgiId')
+            ->all();
+        $data = [];
+        return json_encode($data);
     }
 }
