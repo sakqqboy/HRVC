691e365e6a70e09167094513d2d6a71c31dce201
sakqqboy <saknakhngam@hotmail.com>
2024-12-25 14:53:59 +0700

    update

--- frontend/modules/home/views/default/waiting_approval.php
+++ frontend/modules/home/views/default/waiting_approval.php
@@ -73,7 +73,6 @@ if (isset($pendingApprove) && count($pendingApprove) > 0) { ?>
                                 <span class="font-size-12"><strong><?= $text ?> : </strong><?= $name ?></span>
                             </li>
                         </a>
-
         <?php
                     }
                 endforeach;
@@ -89,28 +88,3 @@ if (isset($pendingApprove) && count($pendingApprove) > 0) { ?>
 <?php
 }
 ?>
-
-<!-- <li class="schedule-item mt-5">
-    <strong>Leave Request</strong> - Employee ID #12<br>
-    <span class="text-muted">Pending since 29/11/2024</span>
-</li>
-<li class="schedule-item mt-5">
-    <strong>Budget Approval</strong> - Marketing Campaign<br>
-    <span class="text-muted">Pending since 25/11/2024</span>
-</li>
-<li class="schedule-item mt-5">
-    <strong>Update KPI</strong> - Non-Japanese Client<br>
-    <span class="text-muted">10:00 AM, 01/12/2024</span>
-</li>
-<li class="schedule-item mt-5">
-    <strong>Submit Report</strong> - Team Performance<br>
-    <span class="text-muted">2:00 PM, 01/12/2024</span>
-</li>
-<li class="schedule-item mt-5">
-    <strong>Update KPI</strong> - Non-Japanese Client<br>
-    <span class="text-muted">10:00 AM, 01/12/2024</span>
-</li>
-<li class="schedule-item mt-5">
-    <strong>Submit Report</strong> - Team Performance<br>
-    <span class="text-muted">2:00 PM, 01/12/2024</span>
-</li> -->
