ebab4112c7c227c5e5f3f3d4c801d4c012e8fc9f
sakqqboy <saknakhngam@hotmail.com>
2025-02-07 09:58:32 +0700

    udpate

--- frontend/modules/kgi/views/management/individual_history.php
+++ frontend/modules/kgi/views/management/individual_history.php
@@ -3,8 +3,8 @@ if (isset($kgiHistoryEmployee) && count($kgiHistoryEmployee) > 0) {
 	foreach ($kgiHistoryEmployee as $kt):
 ?>
 		<li class="schedule-item mt-5" role="button" tabindex="0">
-			<div style="display: flex; justify-content: space-between; align-items: center; width: 100%;">
-				<div style="display: flex; gap: 16px; align-items: center;">
+			<div class="row" style="display: flex; justify-content: space-between; align-items: center;">
+				<div class="col-5" style="display: flex; gap: 16px; align-items: center;">
 					<div style="display: flex; justify-content: center; align-items: center;">
 						<div class="col-5">
 							<img src="<?= Yii::$app->homeUrl ?><?= $kt['picture'] ?>" class="width-ehsan-small" id="picture-history">
@@ -16,19 +16,19 @@ if (isset($kgiHistoryEmployee) && count($kgiHistoryEmployee) > 0) {
 						</span>
 					</div>
 				</div>
-
-				<div style="display: flex; justify-content: center; align-items: center; background-color: rgb(215, 235, 255); border: 0.795px solid #2580D3; border-radius: 36px; padding: 3px 20px; z-index: 1;">
-					<div style="display: flex; justify-content: center; align-items: center; gap: 8px;">
-						<div class="cycle-current">
-							<img src="<?= Yii::$app->homeUrl ?>image/teams.svg" alt="icon">
+				<div class="col-3">
+					<div style="display: flex; justify-content: center; align-items: center; background-color: rgb(215, 235, 255); border: 0.795px solid #2580D3; border-radius: 36px; padding: 3px 20px; z-index: 1;">
+						<div style="display: flex; justify-content: center; align-items: center; gap: 8px;">
+							<div class="cycle-current">
+								<img src="<?= Yii::$app->homeUrl ?>image/teams.svg" alt="icon">
+							</div>
+							<span class="text-black" id="teamName-history" style="font-size: 16px; font-weight: 500;">
+								<?= $kt["teamName"] ?>
+							</span>
 						</div>
-						<span class="text-black" id="teamName-history" style="font-size: 16px; font-weight: 500;">
-							<?= $kt["teamName"] ?>
-						</span>
 					</div>
 				</div>
-
-				<div style="display: flex; flex-direction: column; text-align: right;">
+				<div class="col-4" style="display: flex; flex-direction: column; text-align: right;">
 					<div>
 						<span class="text-gray" id="target-history" style="font-size: 18px; font-weight: 400;">
 							<?= $kt["target"] ?>
