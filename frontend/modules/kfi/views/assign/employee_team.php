4192bba4397ffbd1e764109e53fb45e1c877f1b1
phumapanther <165149554+phumapanther@users.noreply.github.com>
2025-02-17 13:35:38 +0700

    update

--- frontend/modules/kfi/views/assign/employee_team.php
+++ frontend/modules/kfi/views/assign/employee_team.php
@@ -1,57 +1,90 @@
 <?php
 if (isset($kfiTeamEmployee) && count($kfiTeamEmployee) > 0) {
-	foreach ($kfiTeamEmployee as $teamId => $kfiEmployee):
-		if (isset($kfiEmployee["team"])) {
+    foreach ($kfiTeamEmployee as $teamId => $kfiEmployee):
+        if (isset($kfiEmployee["team"])) {
 ?>
-			<div class="col-12 bg-header-assign pb-0 pt-0 pr-8 mt-10" id="team-employee-<?= $teamId ?>">
-				<div class="row">
-					<div class="col-5 font-size-12 pt-5 pb-3">
-						<img src="<?= Yii::$app->homeUrl ?>images/icons/Settings/team-black.png" style="width:15px;margin-top:-3px;" class="ml-5 mr-5">
-						<b><?= $kfiEmployee["team"]["teamName"] ?>, </b><span class="col-12 font-size-10">
-							<?= $kfiEmployee["team"]["departmentName"] ?></span>
-					</div>
-					<div class="col-7 font-size-12 pt-5 pb-3 text-center">
-
-						<img src="<?= Yii::$app->homeUrl ?>images/icons/Settings/blue-down.png" style="width:15px;margin-top:0px;float:right;cursor:pointer;" class="ml-5" onclick="javascript:showEmployeeTeamTarget(<?= $teamId ?>)" id="show-<?= $teamId ?>">
-						<img src="<?= Yii::$app->homeUrl ?>images/icons/Settings/blue-up.png" style="width:15px;margin-top:0px;float:right;display:none;cursor:pointer;" class="ml-5" onclick="javascript:hideEmployeeTeamTarget(<?= $teamId ?>)" id="hide-<?= $teamId ?>">
-					</div>
-				</div>
-			</div>
-		<?php
-		}
-		?>
-		<div class="col-12 pr-0 pl-0" id="employee-in-team-<?= $teamId ?>" style="display:none;">
-			<?php
-			if (isset($kfiEmployee["employee"]) && count($kfiEmployee["employee"]) > 0) {
-				foreach ($kfiEmployee["employee"] as $employeeId => $employee):
-			?>
-					<div class="col-12 bg-white border-bottom">
-						<div class="row">
-							<div class="col-5 font-size-12 pt-5">
-								<div class="row">
-									<div class="col-2 text-center pr-0 pl-0 pt-10">
-										<input type="checkbox" class="from-check ml-10" <?= $employee["checked"] ?> name="employee[<?= $employeeId ?>]">
-									</div>
-									<div class="col-2 pr-5 pl-0 text-center">
-										<img src="<?= Yii::$app->homeUrl ?><?= $employee["picture"] ?>" class="employee-pic-circle">
-									</div>
-									<div class="col-8 pl-5 pt-5">
-										<span class="font-size-12"><b><?= $employee["employeeFirstname"] ?> <?= $employee["employeeSurename"] ?></b></span>
-									</div>
-								</div>
-							</div>
-							<div class="col-5 font-size-12 text-start pt-5">
-								<?= Yii::t('app', $employee["titleName"]) ?>
-							</div>
-
-						</div>
-					</div>
-			<?php
-				endforeach;
-			}
-			?>
-		</div>
+<div class="col-12 bg-header-assign pb-0 pt-0 pr-8 mt-10" id="team-employee-<?= $teamId ?>">
+    <div class="row">
+        <div class="col-3">
+            <div class="pl-10">
+                <input type="checkbox" class="check-all-<?= $teamId ?> " onclick="checkAllEmployees(<?= $teamId ?>)"
+                    style="top: 2px;">
+                <label for=" check-all-<?= $teamId ?>" class="font-size-12">Cleck All</label>
+            </div>
+        </div>
+        <div class="col-4 font-size-12 pt-5 pb-3">
+            <img src="<?= Yii::$app->homeUrl ?>images/icons/Settings/team-black.png" style="width:15px;margin-top:-3px;"
+                class="ml-5 mr-5">
+            <b><?= $kfiEmployee["team"]["teamName"] ?>, </b><span class="col-12 font-size-10">
+                <?= $kfiEmployee["team"]["departmentName"] ?></span>
+        </div>
+        <div class="col-5 font-size-12 pt-5 pb-3 text-center">
+            <img src="<?= Yii::$app->homeUrl ?>images/icons/Settings/blue-down.png"
+                style="width:15px;margin-top:0px;float:right;cursor:pointer;" class="ml-5"
+                onclick="javascript:showEmployeeTeamTarget(<?= $teamId ?>)" id="show-<?= $teamId ?>">
+            <img src="<?= Yii::$app->homeUrl ?>images/icons/Settings/blue-up.png"
+                style="width:15px;margin-top:0px;float:right;display:none;cursor:pointer;" class="ml-5"
+                onclick="javascript:hideEmployeeTeamTarget(<?= $teamId ?>)" id="hide-<?= $teamId ?>">
+        </div>
+    </div>
+</div>
 <?php
-	endforeach;
+        }
+        ?>
+<div class="col-12 pr-0 pl-0" id="employee-in-team-<?= $teamId ?>" style="display:none;">
+    <?php
+        if (isset($kfiEmployee["employee"]) && count($kfiEmployee["employee"]) > 0) {
+            foreach ($kfiEmployee["employee"] as $employeeId => $employee):
+    ?>
+    <div class="col-12 bg-white border-bottom">
+        <div class="row">
+            <div class="col-5 font-size-12 pt-5">
+                <div class="row">
+                    <div class="col-2 text-center pr-0 pl-0 pt-10">
+                        <input type="checkbox" class="from-check ml-10 employee-checkbox-<?= $teamId ?>"
+                            <?= $employee["checked"] ?> name="employee[<?= $employeeId ?>]"
+                            id="employee-checkbox-<?= $employeeId ?>">
+                    </div>
+                    <div class="col-2 pr-5 pl-0 text-center">
+                        <img src="<?= Yii::$app->homeUrl ?><?= $employee["picture"] ?>" class="employee-pic-circle">
+                    </div>
+                    <div class="col-8 pl-5 pt-5">
+                        <span class="font-size-12"><b><?= $employee["employeeFirstname"] ?>
+                                <?= $employee["employeeSurename"] ?></b></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 font-size-12 text-start pt-5">
+                <?= Yii::t('app', $employee["titleName"]) ?>
+            </div>
+
+        </div>
+    </div>
+    <?php
+            endforeach;
+        }
+    ?>
+</div>
+<?php
+    endforeach;
 }
 ?>
+
+<script>
+// JavaScript ฟังก์ชันสำหรับ Check All และ Uncheck All ด้วยการคลิกที่ checkbox
+function checkAllEmployees(teamId) {
+    var checkboxes = document.querySelectorAll('#employee-in-team-' + teamId + ' input[type="checkbox"]');
+    var checkAllCheckbox = document.querySelector('.check-all-' + teamId);
+
+    // ตรวจสอบสถานะของเช็คบ็อกซ์ทั้งหมดในทีม
+    if (checkAllCheckbox.checked) {
+        checkboxes.forEach(function(checkbox) {
+            checkbox.checked = true; // เลือกทั้งหมด
+        });
+    } else {
+        checkboxes.forEach(function(checkbox) {
+            checkbox.checked = false; // ยกเลิกการเลือกทั้งหมด
+        });
+    }
+}
+</script>
