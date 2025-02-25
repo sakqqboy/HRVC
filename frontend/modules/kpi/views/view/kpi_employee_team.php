ae7e490e0899c687aec84bd1a2e35c511fdfa31e
phumapanther <165149554+phumapanther@users.noreply.github.com>
2025-02-25 15:52:19 +0700

    update

--- frontend/modules/kpi/views/view/kpi_employee_team.php
+++ frontend/modules/kpi/views/view/kpi_employee_team.php
@@ -7,32 +7,32 @@
             <div class="col-9  sub-tab">
             </div>
         </div>
-        <div class="col-12 mt-15 pt-0" style="height:295px;overflow-y: auto;">
+        <div class="col-12 mt-15 pt-0" style="height:420px;overflow-y: auto;">
             <div class="row">
                 <?php
                 if (isset($kpiTeams) && count($kpiTeams) > 0) {
                     foreach ($kpiTeams as $teamId => $team): ?>
-                        <div class="col-lg-6 col-md-6 col-12 mb-10">
-                            <div class="col-12 small-content bg-white pl-20">
-                                <div class="row">
-                                    <div class="col-2  pl-0 pr-0">
-                                        <img src="<?= Yii::$app->homeUrl ?>images/icons/Settings/team.svg"
-                                            class="image-AssignMembers">
-                                    </div>
-                                    <div class="col-7 pl-10 border-right">
-                                        <div class="col-12 font-size-12 text-b pr-0">
-                                            <strong><?= $team['teamName'] ?></strong>
-                                        </div>
-                                        <div class="col-12 pim-employee-title" style="font-size: 10px !important;">
-                                            <?= $team["departmentName"] ?>
-                                        </div>
-                                    </div>
-                                    <div class="col-2 text-center pr-0 pl-0 pt-8" style="font-weight: 400;">
-                                        <?= $team['totalEmployee'] ?>
-                                    </div>
+                <div class="col-lg-6 col-md-6 col-12 mb-10">
+                    <div class="col-12 small-content bg-white pl-20">
+                        <div class="row">
+                            <div class="col-2  pl-0 pr-0">
+                                <img src="<?= Yii::$app->homeUrl ?>images/icons/Settings/team.svg"
+                                    class="image-AssignMembers">
+                            </div>
+                            <div class="col-7 pl-10 border-right">
+                                <div class="col-12 font-size-12 text-b pr-0">
+                                    <strong><?= $team['teamName'] ?></strong>
+                                </div>
+                                <div class="col-12 pim-employee-title" style="font-size: 10px !important;">
+                                    <?= $team["departmentName"] ?>
                                 </div>
                             </div>
+                            <div class="col-2 text-center pr-0 pl-0 pt-8" style="font-weight: 400;">
+                                <?= $team['totalEmployee'] ?>
+                            </div>
                         </div>
+                    </div>
+                </div>
                 <?php
                     endforeach;
                 }
@@ -50,28 +50,28 @@
             <div class="col-9  sub-tab">
             </div>
         </div>
-        <div class="col-12 alert bg-white mt-15 pt-0" style="height:280px;overflow-y: auto;">
+        <div class="col-12 bg-white mt-15 pt-0" style="height:420px;overflow-y: auto;">
             <div class="row">
                 <?php
                 if (isset($kpiDetail["kpiEmployeeDetail"]) && count($kpiDetail["kpiEmployeeDetail"]) > 0) {
                     foreach ($kpiDetail["kpiEmployeeDetail"] as $employeeId => $employee): ?>
-                        <div class="col-lg-4 col-md-6 col-12 mt-10 pt-0"
-                            onclick="javascription:openKpiEmployeeView(<?= $employeeId ?>,<?= $kpiId ?>)"
-                            style="cursor: pointer;">
-                            <div class="row">
-                                <div class="col-3 pr-0 pl-0">
-                                    <img src="<?= Yii::$app->homeUrl . $employee['picture'] ?>" class="image-AssignMembers">
-                                </div>
-                                <div class="col-9 pl-10">
-                                    <div class="col-12 pim-employee-Name pr-0">
-                                        <strong><?= $employee['name'] ?></strong>
-                                    </div>
-                                    <div class="col-12 pim-employee-title">
-                                        <?= Yii::t('app', $employee['title']) ?>
-                                    </div>
-                                </div>
+                <div class="col-lg-4 col-md-6 col-12 mt-10 pt-0"
+                    onclick="javascription:openKpiEmployeeView(<?= $employeeId ?>,<?= $kpiId ?>)"
+                    style="cursor: pointer;">
+                    <div class="row">
+                        <div class="col-3 pr-0 pl-0">
+                            <img src="<?= Yii::$app->homeUrl . $employee['picture'] ?>" class="image-AssignMembers">
+                        </div>
+                        <div class="col-9 pl-10">
+                            <div class="col-12 pim-employee-Name pr-0">
+                                <strong><?= $employee['name'] ?></strong>
+                            </div>
+                            <div class="col-12 pim-employee-title">
+                                <?= Yii::t('app', $employee['title']) ?>
                             </div>
                         </div>
+                    </div>
+                </div>
                 <?php
                     endforeach;
                 }
