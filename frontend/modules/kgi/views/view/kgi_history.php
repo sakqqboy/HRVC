f168cd645325184c9eac90e96a5c8467d62d5b45
sakqqboy <saknakhngam@hotmail.com>
2025-02-26 15:07:25 +0700

    update

--- frontend/modules/kgi/views/view/kgi_history.php
+++ frontend/modules/kgi/views/view/kgi_history.php
@@ -25,7 +25,7 @@ $this->title = 'KGI View';
                     </a>
                     <?= $kgiDetail["kgiName"] ?>
                 </div>
-                <div class="col-1">
+                <div class="col-1 text-end">
                     <?php if ($role >= 5) {
                     ?>
                         <a class="btn btn-bg-red-xs" data-bs-toggle="modal" data-bs-target="#delete-kgi"
