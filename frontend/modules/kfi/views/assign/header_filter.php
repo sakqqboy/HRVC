3e8734c3201b663820147fb3e6ded654f3d4258d
phumapanther <165149554+phumapanther@users.noreply.github.com>
2025-04-08 17:13:02 +0700

    update

--- frontend/modules/kfi/views/assign/header_filter.php
+++ frontend/modules/kfi/views/assign/header_filter.php
@@ -1,33 +1,35 @@
 <div class="alert alert-white-4">
-	<div class="row header-filter-pim">
-		<div class="col-12 pt-13 pb-5 pl-0">
-			<div class="row">
-				<div class="col-3 border-right text-center">
-					<a href="<?= Yii::$app->homeUrl ?>kfi/management/grid" class="header-kfi-active">
-						<span>
-							<img src="<?= Yii::$app->homeUrl ?>images/icons/black-icons/FinancialSystem/Group289864.png" class="home-icon mr-5">
-						</span>
-						<?= Yii::t('app', 'Key Financial Indicator') ?>
-					</a>
-				</div>
-				<div class="col-3 border-right text-center">
-					<a href="<?= Yii::$app->homeUrl ?>kgi/management/grid" class="header-kfi">
-						<span>
-							<img src="<?= Yii::$app->homeUrl ?>images/icons/black-icons/FinancialSystem/Vector-1.png" class="home-icon mr-5">
-						</span>
-						<?= Yii::t('app', 'Key Goal Indicator') ?>
-					</a>
-				</div>
-				<div class="col-3 text-center">
-					<a href="<?= Yii::$app->homeUrl ?>kpi/management/grid" class="header-kfi">
-						<span>
-							<img src="<?= Yii::$app->homeUrl ?>images/icons/Dark/48px/KPI.png" class="home-icon mr-5">
-						</span>
-						<?= Yii::t('app', 'Key Performance Indicator') ?>
-					</a>
-				</div>
-			</div>
-		</div>
+    <div class="row header-filter-pim">
+        <div class="col-8 ">
+            <div style="display: inline-flex; align-items: center; gap: 19px;">
+                <div class=" text-center">
+                    <a href="<?= Yii::$app->homeUrl ?>kfi/management/grid" class="header-kfi-active">
+                        <span>
+                            <img src="<?= Yii::$app->homeUrl ?>images/icons/black-icons/FinancialSystem/Group289864.png"
+                                class="home-icon mr-5">
+                        </span>
+                        <?= Yii::t('app', 'Key Financial Indicator') ?>
+                    </a>
+                </div>
+                <div class="  border-left text-center">
+                    <a href="<?= Yii::$app->homeUrl ?>kgi/management/grid" class="header-kfi">
+                        <span>
+                            <img src="<?= Yii::$app->homeUrl ?>images/icons/black-icons/FinancialSystem/Vector-1.png"
+                                class="home-icon mr-5">
+                        </span>
+                        <?= Yii::t('app', 'Key Goal Indicator') ?>
+                    </a>
+                </div>
+                <div class=" border-left text-center">
+                    <a href="<?= Yii::$app->homeUrl ?>kpi/management/grid" class="header-kfi">
+                        <span>
+                            <img src="<?= Yii::$app->homeUrl ?>images/icons/Dark/48px/KPI.png" class="home-icon mr-5">
+                        </span>
+                        <?= Yii::t('app', 'Key Performance Indicator') ?>
+                    </a>
+                </div>
+            </div>
+        </div>
 
-	</div>
+    </div>
 </div>
