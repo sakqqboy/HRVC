ffbadbd5d073586affc90e55b6823fc2906c29a4
phumapanther <165149554+phumapanther@users.noreply.github.com>
2025-07-31 10:32:51 +0700

    ีupdate

--- frontend/modules/kpi/views/kpi-team/header_filter.php
+++ frontend/modules/kpi/views/kpi-team/header_filter.php
@@ -10,18 +10,18 @@
             </a>
         </div>
         <div class="pim-center-line"></div>
-        <div class="pim-type-box" <a href="<?= Yii::$app->homeUrl ?>kgi/kgi-team/team-kgi-grid"
-            style="text-decoration: none;color: #3C3D48;">
-            <span>
-                <img src="<?= Yii::$app->homeUrl ?>images/icons/black-icons/FinancialSystem/KGI.svg"
-                    class="mr-5 pim-filter-head-icon">
-            </span>
-            <?= Yii::t('app', 'Key Goal Indicator') ?>
+        <div class="pim-type-box" style="min-width:182px;">
+            <a href="<?= Yii::$app->homeUrl ?>kgi/management/grid" style="text-decoration: none;color: #3C3D48;">
+                <span>
+                    <img src="<?= Yii::$app->homeUrl ?>images/icons/black-icons/FinancialSystem/KGI.svg"
+                        class="mr-5 pim-filter-head-icon">
+                </span>
+                <?= Yii::t('app', 'Key Goal Indicator') ?>
             </a>
         </div>
         <div class="pim-center-line"></div>
-        <div class="header-kpi-active" style="min-width:235px;">
-            <a href="<?= Yii::$app->homeUrl ?>kpi/kpi-team/team-kpi-grid" style="text-decoration: none;color: #30313D;">
+        <div class="header-kpi-active">
+            <a href="<?= Yii::$app->homeUrl ?>kpi/management/grid" style="text-decoration: none;color: #30313D;">
                 <span>
                     <img src="<?= Yii::$app->homeUrl ?>images/icons/black-icons/FinancialSystem/KPI.svg"
                         class="mr-5 pim-filter-head-icon">
