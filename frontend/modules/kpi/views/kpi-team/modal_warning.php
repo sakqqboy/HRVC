12abd190e92d87301950e5b075fa916963f96081
phumapanther <165149554+phumapanther@users.noreply.github.com>
2025-08-08 14:18:29 +0700

    update

--- frontend/modules/kpi/views/kpi-team/modal_warning.php
+++ frontend/modules/kpi/views/kpi-team/modal_warning.php
@@ -1,6 +1,14 @@
+<style>
+.modal-dialog.warning-kpi-modal {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+}
+</style>
 <div class="modal fade pr-0 pl-0" id="warning-kpi" tabindex="-1" aria-labelledby="staticBackdrop4Label"
     aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered pr-0 pl-0">
+    <div class="modal-dialog modal-dialog-centered warning-kpi-modal pr-0 pl-0">
         <div class="modal-content" style="border-radius: 8px; padding: 20px; width:800px;">
             <div class="d-flex" style="border-bottom: none; padding-bottom: 0;width:100%">
                 <div class="modal-title pull-left font-size-36" id="staticBackdrop4Label"
@@ -15,8 +23,8 @@
 
             </div>
             <div class="modal-body text-start mt-20" style="font-size: 24px; color: #6c757d; padding-top: 10px;line-height: 35px;
-    letter-spacing: 0.5px;">
-                <?= Yii::t('app', "There are some persons in team who have not updated the status to completed yet, in order for to completed, all members must update the status of this personal kgi to complete!") ?>.
+                    letter-spacing: 0.5px;">
+                <?= Yii::t('app', "There are some persons in team who have not updated the status to completed yet, in order for to completed, all members must update the status of this personal kpi to complete!") ?>.
             </div>
             <div class="modal-footer text-end w-100 pt-0 pb-0" style="border-top: none;">
                 <a class="btn btn-outline-danger text-center pt-0 pb-0" data-bs-dismiss="modal" aria-label="Close"
@@ -27,18 +35,3 @@
         </div>
     </div>
 </div>
-<style>
-/* @media (max-width: 1935px) and (max-height: 950px) {
-        .modal-content {
-            transform: scale(1) !important;
-            transform-origin: top left;
-            width: calc(100% / 1);
-        }
-
-        .modal-dialog {
-            transform-origin: top left;
-            width: calc(100% / 0.95);
-            left: 0;
-        }
-    } */
-</style>
