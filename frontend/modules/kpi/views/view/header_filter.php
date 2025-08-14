1d0e7eb870bf5fe766711bf6c5408f85cfaf4b84
phumapanther <165149554+phumapanther@users.noreply.github.com>
2025-08-14 17:31:11 +0700

    update

--- frontend/modules/kpi/views/view/header_filter.php
+++ frontend/modules/kpi/views/view/header_filter.php
@@ -1,15 +1,5 @@
 <div class="d-flex header-filter-pim">
     <div class="pim-head-upderline d-flex align-items-start justify-content-start">
-        <div class="pim-type-box" style="min-width:206px;">
-            <a href="<?= Yii::$app->homeUrl ?>kfi/management/grid" style="text-decoration: none;color: #30313D;">
-                <span>
-                    <img src="<?= Yii::$app->homeUrl ?>images/icons/black-icons/FinancialSystem/KFI.svg"
-                        class="mr-5 pim-filter-head-icon">
-                </span>
-                <?= Yii::t('app', 'Key Financial Indicator') ?>
-            </a>
-        </div>
-        <div class="pim-center-line"></div>
         <div class="pim-type-box" style="min-width:182px;">
             <a href="<?= Yii::$app->homeUrl ?>kgi/management/grid" style="text-decoration: none;color: #3C3D48;">
                 <span>
