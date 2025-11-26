88161ef02c8c2acae92ec5b1346fd600fbf26d57
phumapanther <165149554+phumapanther@users.noreply.github.com>
2025-11-26 17:08:04 +0700

    update

--- frontend/modules/kfi/views/management/kfi_from.php
+++ frontend/modules/kfi/views/management/kfi_from.php
@@ -132,7 +132,7 @@ if ($DueBehind < 0) {
                         </text>
                     </a> -->
                     <div class="pim-name-title" style="display: flex; align-items: center; gap: 14px;">
-                         <a href="<?= Yii::$app->request->referrer ? Yii::$app->request->referrer : Yii::$app->homeUrl . 'kfi/management/grid' ?>" style="text-decoration: none; width:66px; height:26px;" class="btn-create-branch">
+                         <a href="<?= isset(Yii::$app->request->referrer) ? Yii::$app->request->referrer : Yii::$app->homeUrl . 'kfi/management/grid' ?>" style="text-decoration: none; width:66px; height:26px;" class="btn-create-branch">
                             <img src="<?= Yii::$app->homeUrl ?>images/icons/Settings/back-white.svg" style="width:18px; height:18px; margin-top:-3px;">
                             <?= Yii::t('app', 'Back') ?>        
                         </a>
--- frontend/modules/kgi/views/kgi-team/kgi_form.php
+++ frontend/modules/kgi/views/kgi-team/kgi_form.php
@@ -171,7 +171,7 @@ if (!$nextCheckDate) {
                         </text>
                     </a> -->
                     <div class="pim-name-title" style="display: flex; align-items: center; gap: 14px;">
-                        <a href="<?= Yii::$app->request->referrer ? Yii::$app->request->referrer : Yii::$app->homeUrl . 'kgi/management/grid' ?>" style="text-decoration: none; width:66px; height:26px;" class="btn-create-branch">
+                        <a href="<?= isset(Yii::$app->request->referrer) ? Yii::$app->request->referrer : Yii::$app->homeUrl . 'kgi/kgi-team/team-kgi-grid' ?>"  style="text-decoration: none; width:66px; height:26px;" class="btn-create-branch">
                             <img src="<?= Yii::$app->homeUrl ?>images/icons/Settings/back-white.svg" style="width:18px; height:18px; margin-top:-3px;">
                             <?= Yii::t('app', 'Back') ?>        
                         </a>
